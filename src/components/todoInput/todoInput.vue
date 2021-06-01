7a888438c9458def0147597e1d2f4003bbf861ba
BlinovaTA <tanyushablinova@gmail.com>
2021-06-02 00:42:01 +0500

    Валидация

--- src/components/todoInput/todoInput.vue
+++ src/components/todoInput/todoInput.vue
@@ -1,18 +1,27 @@
 <template lang="pug">
   .todo-input
+    .error {{validation.firstError('todo.name')}}
     input(
       type="text"
       placeholder="Todo Name"
       autofocus
       v-model="todo.name"
+      :class="{'valid-error' : validation.hasError('todo.name')}"
       @keydown.enter="addTodo"
     ).input
 </template>
 
 <script>
 import uniqid from "uniqid";
+import { Validator } from "simple-vue-validator";
 
 export default {
+  mixins: [require("simple-vue-validator").mixin],
+  validators: {
+    "todo.name"(value) {
+      return Validator.value(value).required("Поле не может быть пустым");
+    }
+  },
   data() {
     return {
       todo: {
@@ -24,9 +33,14 @@ export default {
   },
   methods: {
     addTodo() {
-      this.todo.id = uniqid();
-      this.$emit("addTodo", {...this.todo});
-      this.todo.name = "";
+      this.$validate().then(success => {
+        if (!success) return;
+        
+        this.todo.id = uniqid();
+        this.$emit("addTodo", {...this.todo});
+        this.todo.name = "";
+        this.validation.reset();
+      })
     }
   }
 }
